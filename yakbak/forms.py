e9d1cbac5f11aa0b6f58eab60f3fdc83ef953d6b
Dan Crosta <dcrosta@late.am>
2018-06-04 08:55:50 -0400

    we don't need the doc/description on form fields

--- yakbak/forms.py
+++ yakbak/forms.py
@@ -6,26 +6,11 @@ from yakbak.models import db, Talk
 from yakbak.types import Application
 
 
-class ModelConverterWithDoc(ModelConverter):
-
-    """
-    Use the ``doc`` attribute of SQLAlchemy columns for WTForms description.
-
-    """
-
-    def convert(self, model, mapper, prop, field_args, db_session=None):  # type: ignore
-        field = super().convert(model, mapper, prop, field_args, db_session)
-        if prop.doc:
-            field.kwargs["description"] = prop.doc
-        return field
-
-
 TalkForm = model_form(
     model=Talk,
     db_session=db.session,
     base_class=FlaskForm,
     exclude={"talk_id", "length", "speakers", "updated", "created"},
-    converter=ModelConverterWithDoc(),
 )
 
 
