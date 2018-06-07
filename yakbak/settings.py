67b4140dd59fb3898be16d8ac01fedff548a5e46
Dan Crosta <dcrosta@late.am>
2018-06-07 07:00:47 -0400

    add helpful flask setting for dev

--- yakbak/settings.py
+++ yakbak/settings.py
@@ -49,6 +49,8 @@ class DbSettings:
 @attrs(frozen=True)
 class FlaskSettings:
     secret_key: str = attrib(validator=instance_of(str))
+    templates_auto_reload: bool = attrib(
+        validator=optional(instance_of(bool)), default=False)
 
 
 @attrs(frozen=True)
