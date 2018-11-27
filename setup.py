pyconca/2019-cfp

commit 859a3352bade46d12ba0d71fbb807d7f40f93702

move conference config to database
eventually we'll support more than one conference per instance; this is
just a step in that direction

setup.py
@@ -27,7 +27,8 @@ setup(
         "python-frontmatter",
         "python-social-auth",
         "social-auth-app-flask-sqlalchemy",
-        "sqlalchemy",
+        "sqlalchemy >= 1.3.0b1",
+        "sqlalchemy-postgresql-json",
         "toml",
     ],
     entry_points={

yakbak/forms.py
@@ -1,18 +1,29 @@
+from typing import Any
+
 from flask_wtf import FlaskForm
+from wtforms import Form
 from wtforms.ext.sqlalchemy.orm import model_form
-from wtforms.fields import SelectField, StringField
+from wtforms.fields import StringField
 from wtforms.validators import Email
 
-from yakbak.models import db, Talk, User
-from yakbak.types import Application
+from yakbak.models import Conference, db, Talk, User
 
 
-TalkForm = model_form(
+_TalkForm: Form = model_form(
     model=Talk,
     db_session=db.session,
     base_class=FlaskForm,
-    exclude={"talk_id", "length", "speakers", "updated", "created"},
+    exclude={"talk_id", "speakers", "updated", "created"},
 )
+
+
+class TalkForm(_TalkForm):
+    def __init__(self, conference: Conference, *args: Any, **kwargs: Any) -> None:
+        self.length.choices = [
+            (length, f"{length} Minutes")
+            for length in conference.talk_lengths
+        ]
+        super().__init__(*args, **kwargs)
 
 
 UserForm = model_form(
@@ -25,15 +36,3 @@ UserForm = model_form(
 
 class MagicLinkForm(FlaskForm):
     email = StringField(validators=[Email()])
-
-
-def init_forms(app: Application) -> None:
-    # Add "length" (talk length) field during app initialization
-    # since its values depend on settings not know at module-time
-    TalkForm.length = SelectField(
-        choices=[
-            (length, f"{length} Minutes")
-            for length in app.settings.cfp.talk_lengths
-        ],
-        coerce=int,
-    )

alembic/versions/126d519970b8_move_conference_settings_to_db.py
@@ -1,0 +1,34 @@
+"""move conference settings to DB
+
+Revision ID: 126d519970b8
+Revises: 219a9aa714da
+Create Date: 2018-11-21 10:01:07.906369
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '126d519970b8'
+down_revision = '219a9aa714da'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.create_table('conference',
+    sa.Column('conference_id', sa.Integer(), nullable=False),
+    sa.Column('full_name', sa.String(length=256), nullable=False),
+    sa.Column('informal_name', sa.String(length=256), nullable=False),
+    sa.Column('talk_lengths', sa.JSON(), nullable=False),
+    sa.PrimaryKeyConstraint('conference_id')
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.drop_table('conference')
+    # ### end Alembic commands ###

yakbak.toml-example
@@ -1,11 +1,3 @@
-[site]
-title="A PyConference"
-conference="PyConference 2018"
-copyright="Copyright &copy; 2018 PyConference Organizers"
-
-[cfp]
-talk_lengths=[25, 40]
-
 [db]
 url="postgres+psycopg2://localhost/yakbak"
 

yakbak/core.py
@@ -4,15 +4,15 @@ import os
 import sys
 
 from attr import asdict
+from flask import g
 from flask_wtf.csrf import CSRFProtect
 from social_flask.routes import social_auth
 from social_flask_sqlalchemy.models import init_social
 
 from yakbak import view_helpers, views
 from yakbak.auth import login_manager
-from yakbak.forms import init_forms
 from yakbak.mail import mail
-from yakbak.models import db
+from yakbak.models import Conference, db
 from yakbak.settings import Settings
 from yakbak.types import Application
 
@@ -47,12 +47,13 @@ def create_app(settings: Settings, flask_config: Dict[str, Any] = {}) -> Applica
     set_up_database(app)
     set_up_auth(app)
     set_up_mail(app)
-    init_forms(app)
     CSRFProtect(app)
 
     app.register_blueprint(views.app)
     app.register_blueprint(view_helpers.app)  # filters etc
     app.register_blueprint(social_auth, url_prefix="/login/external")
+
+    set_up_handlers(app)
 
     return app
 
@@ -144,3 +145,10 @@ def set_up_mail(app: Application) -> None:
     app.config["MAIL_MAX_EMAILS"] = 10  # guess
 
     mail.init_app(app)
+
+
+def set_up_handlers(app: Application) -> None:
+    @app.before_request
+    def load_conference() -> None:
+        # TODO: load by URL or something
+        g.conference = Conference.query.one()

yakbak/flaskcli.py
@@ -1,7 +1,9 @@
 import os.path
 
+import click
+
 from yakbak.core import create_app
-from yakbak.models import db
+from yakbak.models import Conference, db
 from yakbak.settings import find_settings_file, load_settings_file
 
 
@@ -23,3 +25,18 @@ def sync_db() -> None:
     # so do this second to creating the yakbak-specific models
     from social_flask_sqlalchemy import models
     models.PSABase.metadata.create_all(db.engine)
+
+
+@app.cli.command()
+@click.argument("full_name")
+@click.argument("informal_name")
+@click.argument("talk_lengths")
+def add_conference(full_name: str, informal_name: str, talk_lengths: str) -> None:
+    lengths = [int(l) for l in talk_lengths.split(",")]
+    conf = Conference(
+        full_name=full_name,
+        informal_name=informal_name,
+        talk_lengths=lengths,
+    )
+    db.session.add(conf)
+    db.session.commit()

yakbak/models.py
@@ -20,15 +20,29 @@ Some style notes:
 
 """
 from datetime import datetime
-from typing import Iterable
+from typing import Iterable, List, Optional
 import logging
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import synonym
+from sqlalchemy.types import JSON
+from sqlalchemy_postgresql_json import JSONMutableList
 
 
 db = SQLAlchemy()
 logger = logging.getLogger("models")
+
+
+class Conference(db.Model):  # type: ignore
+    conference_id: int = db.Column(db.Integer, primary_key=True)
+
+    full_name: str = db.Column(db.String(256), nullable=False)
+    informal_name: str = db.Column(db.String(256), nullable=False)
+
+    talk_lengths: List[int] = db.Column(
+        JSONMutableList.as_mutable(JSON),
+        nullable=False,
+    )
 
 
 talk_speaker = db.Table(
@@ -89,9 +103,9 @@ class Talk(db.Model):  # type: ignore
 
     title: str = db.Column(db.String(512), nullable=False)
     length: int = db.Column(db.Integer, nullable=False)
-    description: str = db.Column(db.Text, nullable=True)
-    outline: str = db.Column(db.Text, nullable=True)
-    requirements: str = db.Column(db.Text, nullable=True)
+    description: Optional[str] = db.Column(db.Text)
+    outline: Optional[str] = db.Column(db.Text)
+    requirements: Optional[str] = db.Column(db.Text)
 
     created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
     updated = db.Column(

yakbak/settings.py
@@ -26,21 +26,6 @@ class AuthMethod:
         return url_for(self.view, **self.view_kwargs)
 
 
-def list_of(validator: ValidationFunc) -> ValidationFunc:
-    def list_validator(instance: Any, attribute: Any, value: Any) -> None:
-        if not isinstance(value, (list, tuple)):
-            raise TypeError("value must be a list or tuple")
-        for element in value:
-            validator(instance, attribute, element)
-
-    return list_validator
-
-
-@attrs(frozen=True)
-class CfpSettings:
-    talk_lengths: List[int] = attrib(validator=list_of(instance_of(int)))
-
-
 @attrs(frozen=True)
 class DbSettings:
     url: str = attrib(validator=instance_of(str))
@@ -56,13 +41,6 @@ class FlaskSettings:
 @attrs(frozen=True)
 class LoggingSettings:
     level: str = attrib(validator=instance_of(str), default="INFO")
-
-
-@attrs(frozen=True)
-class SiteSettings:
-    title: str = attrib(validator=instance_of(str))
-    conference: str = attrib(validator=instance_of(str))
-    copyright: str = attrib(validator=instance_of(str))
 
 
 @attrs(frozen=True)
@@ -136,11 +114,9 @@ class SMTPSettings:
 @attrs(frozen=True)
 class Settings:
     auth: AuthSettings = attrib()
-    cfp: CfpSettings = attrib()
     db: DbSettings = attrib()
     flask: FlaskSettings = attrib()
     logging: LoggingSettings = attrib()
-    site: SiteSettings = attrib()
     smtp: SMTPSettings = attrib()
 
 

commit 7fbb9962aeeee2d3e49ee0699ce201d4beb0032d

use wtforms-alchemhy
this gives a nicer, more declarative form interface, and fixes a bug
introduced in 859a3352bade46d12ba0d71fbb807d7f40f93702 which broke the
talk length field (it became a text input, should be a select field)

setup.py
@@ -30,6 +30,7 @@ setup(
         "sqlalchemy >= 1.3.0b1",
         "sqlalchemy-postgresql-json",
         "toml",
+        "wtforms-alchemy",
     ],
     entry_points={
         "flask.commands": [

yakbak/forms.py
@@ -1,37 +1,47 @@
-from typing import Any
+from typing import Iterable, Tuple
 
+from flask import g
 from flask_wtf import FlaskForm
-from wtforms import Form
-from wtforms.ext.sqlalchemy.orm import model_form
-from wtforms.fields import StringField
-from wtforms.validators import Email
+from wtforms.fields import SelectField, StringField
+from wtforms.validators import DataRequired, Email
+from wtforms_alchemy import model_form_factory
 
-from yakbak.models import Conference, db, Talk, User
-
-
-_TalkForm: Form = model_form(
-    model=Talk,
-    db_session=db.session,
-    base_class=FlaskForm,
-    exclude={"talk_id", "speakers", "updated", "created"},
-)
+from yakbak.models import Talk, User
 
 
-class TalkForm(_TalkForm):
-    def __init__(self, conference: Conference, *args: Any, **kwargs: Any) -> None:
-        self.length.choices = [
+ModelForm = model_form_factory(FlaskForm)  # type: FlaskForm
+
+
+class TalkLengthChoices:
+    """
+    Defer the determination of choices for talk lengths to runtime.
+
+    Because each conference might have different talk lengths, we can't
+    pre-define the choices in the way WTF usually wants for a select field,
+    so we use this trickery instead.
+    """
+    def __iter__(self) -> Iterable[Tuple[int, str]]:
+        return iter([
             (length, f"{length} Minutes")
-            for length in conference.talk_lengths
-        ]
-        super().__init__(*args, **kwargs)
+            for length in g.conference.talk_lengths
+        ])
 
 
-UserForm = model_form(
-    model=User,
-    db_session=db.session,
-    base_class=FlaskForm,
-    exclude={"user_id", "email", "talks", "created", "updated"},
-)
+class TalkForm(ModelForm):
+    class Meta:
+        model = Talk
+
+    length = SelectField(
+        coerce=int,
+        choices=TalkLengthChoices(),
+        validators=[DataRequired()],
+    )
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        exclude = {"email"}
 
 
 class MagicLinkForm(FlaskForm):