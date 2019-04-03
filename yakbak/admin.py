85c045d9cef02875e81ed6b7934b53c5332678aa
Dan Crosta <dcrosta@late.am>
2019-04-03 08:11:20 -0400

    add very basic stats to admin dashboard

--- yakbak/admin.py
+++ yakbak/admin.py
@@ -1,11 +1,46 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 
-from yakbak.models import Conference, db, Talk, User
+from yakbak.forms import Ethnicity, Gender
+from yakbak.models import Conference, db, DemographicSurvey, Talk, User
 
 
-admin = Admin(template_mode="bootstrap3")
+class AdminDashboard(AdminIndexView):
+
+    @expose("/")
+    def dashboard(self):
+        num_talks = Talk.query.count()
+
+        # TODO: figure out how to do this with "not in JSON list" queires
+        num_surveys = 0
+        num_non_man = 0
+        num_non_white = 0
+        for survey in DemographicSurvey.query.all():
+            num_surveys += 1
+            if Gender.MAN.name not in survey.gender:
+                num_non_man += 1
+            if Ethnicity.WHITE_CAUCASIAN.name not in survey.ethnicity:
+                num_non_white += 1
+
+        return self.render(
+            "admin/index.html",
+            num_talks=num_talks,
+            num_surveys=num_surveys,
+            num_non_man=num_non_man,
+            num_non_white=num_non_white,
+        )
+
+
+admin = Admin(index_view=AdminDashboard(), template_mode="bootstrap3")
 
 admin.add_view(ModelView(Conference, db.session))
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Talk, db.session))
+
+class DemographicSurveyView(ModelView):
+    column_exclude_list = ("user", )
+
+    def __init__(self):
+        super().__init__(DemographicSurvey, db.session)
+
+admin.add_view(DemographicSurveyView())
