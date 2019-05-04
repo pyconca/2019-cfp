3581160faca4e77bee28241be02b9abb40b20420
Dan Crosta <dcrosta@late.am>
2019-05-04 11:14:27 -0400

    include Apache 2.0 text in vendored code from diff-match-patch

--- yakbak/diff.py
+++ yakbak/diff.py
@@ -1,7 +1,23 @@
 # Per Google's recommendation [1], this is copied from [2], with
 # the line ending match adjusted to find spans of whitespace.
 #
-# The original [2] is used under the Apache License, Version 2.0.
+# The original [2] is used under the Apache License, Version 2.0:
+#
+#   Diff Match and Patch
+#   Copyright 2018 The diff-match-patch Authors.
+#   https://github.com/google/diff-match-patch
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 #
 # [1] https://github.com/google/diff-match-patch/wiki/Line-or-Word-Diffs#word-mode
 # [2] https://github.com/google/diff-match-patch/blob/858b3812cc02e7d48da4beebb21d4d80dc1d3062/python3/diff_match_patch.py
