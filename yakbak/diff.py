0155b8dcf0e103fe38a3e7b22b73572fe877bcd9
Dan Crosta <dcrosta@late.am>
2019-05-04 10:54:09 -0400

    vendor in diff-match-patch's diff_linesToChars
    we'll modify thihs as described in diff-match-patch's wiki to support
    word-diffing in future commits; checking in the original version here
    so that we can track changes with git history
    
    copied from https://raw.githubusercontent.com/google/diff-match-patch/858b3812cc02e7d48da4beebb21d4d80dc1d3062/python3/diff_match_patch.py

---
+++ yakbak/diff.py
@@ -0,0 +1,67 @@
+# This is copied from [1], and is used under the Apache License, Version 2.0.
+#
+# [1] https://github.com/google/diff-match-patch/blob/858b3812cc02e7d48da4beebb21d4d80dc1d3062/python3/diff_match_patch.py
+
+
+def diff_linesToChars(self, text1, text2):
+  """Split two texts into an array of strings.  Reduce the texts to a string
+  of hashes where each Unicode character represents one line.
+
+  Args:
+    text1: First string.
+    text2: Second string.
+
+  Returns:
+    Three element tuple, containing the encoded text1, the encoded text2 and
+    the array of unique strings.  The zeroth element of the array of unique
+    strings is intentionally blank.
+  """
+  lineArray = []  # e.g. lineArray[4] == "Hello\n"
+  lineHash = {}   # e.g. lineHash["Hello\n"] == 4
+
+  # "\x00" is a valid character, but various debuggers don't like it.
+  # So we'll insert a junk entry to avoid generating a null character.
+  lineArray.append('')
+
+  def diff_linesToCharsMunge(text):
+    """Split a text into an array of strings.  Reduce the texts to a string
+    of hashes where each Unicode character represents one line.
+    Modifies linearray and linehash through being a closure.
+
+    Args:
+      text: String to encode.
+
+    Returns:
+      Encoded string.
+    """
+    chars = []
+    # Walk the text, pulling out a substring for each line.
+    # text.split('\n') would would temporarily double our memory footprint.
+    # Modifying text would create many large strings to garbage collect.
+    lineStart = 0
+    lineEnd = -1
+    while lineEnd < len(text) - 1:
+      lineEnd = text.find('\n', lineStart)
+      if lineEnd == -1:
+        lineEnd = len(text) - 1
+      line = text[lineStart:lineEnd + 1]
+
+      if line in lineHash:
+        chars.append(chr(lineHash[line]))
+      else:
+        if len(lineArray) == maxLines:
+          # Bail out at 1114111 because chr(1114112) throws.
+          line = text[lineStart:]
+          lineEnd = len(text)
+        lineArray.append(line)
+        lineHash[line] = len(lineArray) - 1
+        chars.append(chr(len(lineArray) - 1))
+      lineStart = lineEnd + 1
+    return "".join(chars)
+
+  # Allocate 2/3rds of the space for text1, the rest for text2.
+  maxLines = 666666
+  chars1 = diff_linesToCharsMunge(text1)
+  maxLines = 1114111
+  chars2 = diff_linesToCharsMunge(text2)
+  return (chars1, chars2, lineArray)
